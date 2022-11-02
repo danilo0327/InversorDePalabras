cea15af433380171e5fd13b9781b7d44a53d1c8c
danilo0327 <danilo.alfonsody@gmail.com>
2022-11-02 09:42:02 -0500

    Creating an Scanner object called (entrada)

--- src/main/java/inversordepalabras/InversorDePalabras.java
+++ src/main/java/inversordepalabras/InversorDePalabras.java
@@ -1,12 +1,18 @@
 
 package inversordepalabras;
 
+import java.util.Scanner;
+
 
 public class InversorDePalabras {
 
     public static void main(String[] args) {
         
+        //Definiendo Variables a usar
         String palabra = "", palabraInvertida = "";
         int longitudPalabra = 0;
+        
+        //Creando objeto Tipo Scanner
+        Scanner entrada = new Scanner(System.in);
     }
 }
