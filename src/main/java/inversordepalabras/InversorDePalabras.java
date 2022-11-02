e026f7e2f0c002a29e97f5f28783143e306aef02
danilo0327 <danilo.alfonsody@gmail.com>
2022-11-02 10:08:09 -0500

    Fixing last details to run the program

--- src/main/java/inversordepalabras/InversorDePalabras.java
+++ src/main/java/inversordepalabras/InversorDePalabras.java
@@ -14,5 +14,21 @@ public class InversorDePalabras {
         
         //Creando objeto Tipo Scanner
         Scanner entrada = new Scanner(System.in);
+        
+        //Texto que se va imprimir para que el usuario ingrese la data al programa.
+        System.out.print("Type a word or sentence: ");
+        palabra = entrada.nextLine();
+        
+        //Definición de variable tipo int que me permite contar los caracteres de una variable String
+        longitudPalabra = palabra.length();
+        
+        //Ciclo while para recorrer el String
+        while(longitudPalabra != 0) {
+            palabraInvertida += palabra.substring(longitudPalabra -1, longitudPalabra);
+            longitudPalabra--; //Decremento de uno en uno.
+        }
+        
+        System.out.print("Word or sentence invested: " +palabraInvertida);
+        System.out.println("");
     }
 }
